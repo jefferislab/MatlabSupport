79b3f76212a7375be050caaf9133528f95a30964
Gregory Jefferis <jefferis@gmail.com>
2010-06-05 22:07:24 +0100

    Eliminate nested functions in impicinfo for Octave compatibility
    * nested functions are not supported in Octave
    * perhaps these is where an object oriented approach
      might be more elegant

--- ReadPIC/impicinfo.m
+++ ReadPIC/impicinfo.m
@@ -5,41 +5,6 @@ function metadata = impicinfo(filename)
 
 % constants
 HEADER_LEN  = 76;
-NOTE_LEN    = 96;
-
-NOTE_TYPE_LIVE = 1;         % Information about live collection
-NOTE_TYPE_FILE1 = 2;        % Note from image #1
-NOTE_TYPE_NUMBER = 3;       % Number in multiple image file
-NOTE_TYPE_USER = 4;         % User notes generated notes
-NOTE_TYPE_LINE = 5;         % Line mode info
-NOTE_TYPE_COLLECT = 6;      % Collect mode info
-NOTE_TYPE_FILE2 = 7;        % Note from image #2
-NOTE_TYPE_SCALEBAR = 8;     % Scale bar info
-NOTE_TYPE_MERGE = 9;        % Merge Info
-NOTE_TYPE_THRUVIEW = 10;    % Thruview Info
-NOTE_TYPE_ARROW = 11;       % Arrow info
-NOTE_TYPE_VARIABLE = 20;    % Again internal variable ,except held as
-NOTE_TYPE_STRUCTURE = 21;   % a structure.
-
-AXT_D = 1;                  % distance in microns
-AXT_T = 2;                  % time in sec
-AXT_A = 3;                  % angle in degrees
-AXT_I = 4;                  % intensity in grey levels
-AXT_M4 = 5;                 % 4-bit merged image
-AXT_R = 6;                  % Ratio
-AXT_LR = 7;                 % Log Ratio
-AXT_P = 8;                  % Product
-AXT_C = 9;                  % Calibrated
-AXT_PHOTON = 10;			% intensity in photons/sec
-AXT_RGB = 11;               % RGB type
-AXT_SEQ = 12;               % SEQ type (eg 'experiments')
-AXT_6D = 13;                % 6th level of axis
-AXT_TC = 14;				% Time Course axis
-AXT_S = 15;                 % Intensity signoid cal
-AXT_LS = 16;				% Intensity log signoid cal
-AXT_BASE = base2dec('FF', 16);	% mask for axis TYPE
-AXT_XY = base2dec('100', 16);	% axis is XY, needs updating by LENS
-AXT_WORD = base2dec('200', 16);  % axis is word. only corresponds to axis[0]
 
 % read header
 fid = fopen(filename, 'r');
@@ -104,7 +69,7 @@ if fid ~= -1
         % read notes
         fseek(fid, metadata.Width * metadata.Height * metadata.NumImages * metadata.BitDepth / 8, 'cof');
         notesOffset = ftell(fid);
-        getAxisInfo;
+        [metadata] = getAxisInfo (fid,notesOffset,metadata,endOfFile);
         fclose(fid);
     else
         metadata = []; %tell calling subroutine that the file was zero length
@@ -113,78 +78,117 @@ else
     metadata = []; %tell calling subroutine that no file was found
 end
 
-    function getAxisInfo
-        % read all of the notes
-        fseek(fid, notesOffset, 'bof');
-        noteIndex = 1;
+end
 
-        while notesOffset + NOTE_LEN * (noteIndex - 1) <= endOfFile
-            metadata.Note{noteIndex} = readNote(noteIndex);
+function [metadata] = getAxisInfo(fid,notesOffset,metadata,endOfFile)
+    NOTE_LEN    = 96;
 
-            if metadata.Note{noteIndex}.type == NOTE_TYPE_VARIABLE
-                if strfind(metadata.Note{noteIndex}.text, 'AXIS_2') == 1
-                    % horizontal axis
-                    tempData = sscanf(metadata.Note{noteIndex}.text(7:end), ' %d %g %g %s');
-                    axisType = tempData(1);
-                    if axisType == AXT_D
-                        metadata.Origin(1) = tempData(2);
-                        metadata.Delta(1) = tempData(3);
-                    end
-                    metadata.Units{1} = char(tempData(4:end)');
-                elseif strfind(metadata.Note{noteIndex}.text, 'AXIS_3') == 1
-                    % vertical axis
-                    tempData = sscanf(metadata.Note{noteIndex}.text(7:end), ' %d %g %g %s');
-                    axisType = tempData(1);
-                    if axisType == AXT_D
-                        metadata.Origin(2) = tempData(2);
-                        metadata.Delta(2) = tempData(3);
-                    end
-                    metadata.Units{2} = char(tempData(4:end)');
-                elseif strfind(metadata.Note{noteIndex}.text, 'AXIS_4') == 1
-                    % z axis
-                    tempData = sscanf(metadata.Note{noteIndex}.text(7:end), ' %d %g %g %s');
-                    axisType = tempData(1);
-                    if axisType == AXT_D
-                        metadata.Origin(3) = tempData(2);
-                        metadata.Delta(3) = tempData(3);
-                    end
-                    metadata.Units{3} = char(tempData(4:end)');
-                elseif strfind(metadata.Note{noteIndex}.text, 'AXIS_9') == 1
-                    tempData = sscanf(metadata.Note{noteIndex}.text(7:end), ' %d %g %g %s');
-                    axisType = tempData(1);
-                    if axisType == AXT_RGB
-                        metadata.Origin(4) = tempData(2);
-                        metadata.Delta(4) = tempData(3);
-                        metadata.Units{4} = char(tempData(4:end)');
-                    end
-                elseif strfind(metadata.Note{noteIndex}.text, 'INFO_FRAME_RATE') == 1
-                    metadata.FramesPerSecond = sscanf(metadata.Note{noteIndex}.text(14:end), ' %d');
-                elseif strfind(metadata.Note{noteIndex}.text, 'INFO_OBJECTIVE_NAME = ') == 1
-                    metadata.Objective = metadata.Note{noteIndex}.text(23:end);
-                    metadata.Objective = metadata.Objective(metadata.Objective ~= char(0));
-                elseif strfind(metadata.Note{noteIndex}.text, 'PIC_FF_VERSION = ') == 1
-                    metadata.FormatVersion = str2double(metadata.Note{noteIndex}.text(18:end));
-                else
-                    % add any note you care about here
+	NOTE_TYPE_LIVE = 1;         % Information about live collection
+	NOTE_TYPE_FILE1 = 2;        % Note from image #1
+	NOTE_TYPE_NUMBER = 3;       % Number in multiple image file
+	NOTE_TYPE_USER = 4;         % User notes generated notes
+	NOTE_TYPE_LINE = 5;         % Line mode info
+	NOTE_TYPE_COLLECT = 6;      % Collect mode info
+	NOTE_TYPE_FILE2 = 7;        % Note from image #2
+	NOTE_TYPE_SCALEBAR = 8;     % Scale bar info
+	NOTE_TYPE_MERGE = 9;        % Merge Info
+	NOTE_TYPE_THRUVIEW = 10;    % Thruview Info
+	NOTE_TYPE_ARROW = 11;       % Arrow info
+	NOTE_TYPE_VARIABLE = 20;    % Again internal variable ,except held as
+	NOTE_TYPE_STRUCTURE = 21;   % a structure.
 
+	AXT_D = 1;                  % distance in microns
+	AXT_T = 2;                  % time in sec
+	AXT_A = 3;                  % angle in degrees
+	AXT_I = 4;                  % intensity in grey levels
+	AXT_M4 = 5;                 % 4-bit merged image
+	AXT_R = 6;                  % Ratio
+	AXT_LR = 7;                 % Log Ratio
+	AXT_P = 8;                  % Product
+	AXT_C = 9;                  % Calibrated
+	AXT_PHOTON = 10;			% intensity in photons/sec
+	AXT_RGB = 11;               % RGB type
+	AXT_SEQ = 12;               % SEQ type (eg 'experiments')
+	AXT_6D = 13;                % 6th level of axis
+	AXT_TC = 14;				% Time Course axis
+	AXT_S = 15;                 % Intensity signoid cal
+	AXT_LS = 16;				% Intensity log signoid cal
+	AXT_BASE = base2dec('FF', 16);	% mask for axis TYPE
+	AXT_XY = base2dec('100', 16);	% axis is XY, needs updating by LENS
+	AXT_WORD = base2dec('200', 16);  % axis is word. only corresponds to axis[0]
+	
+	% read all of the notes
+    fseek(fid, notesOffset, 'bof');
+    noteIndex = 1;
+
+    while notesOffset + NOTE_LEN * (noteIndex - 1) <= endOfFile
+        metadata.Note{noteIndex} = readNote(fid,noteIndex,notesOffset);
+
+        if metadata.Note{noteIndex}.type == NOTE_TYPE_VARIABLE
+            if strfind(metadata.Note{noteIndex}.text, 'AXIS_2') == 1
+                % horizontal axis
+                tempData = sscanf(metadata.Note{noteIndex}.text(7:end), ' %d %g %g %s');
+                axisType = tempData(1);
+                if axisType == AXT_D
+                    metadata.Origin(1) = tempData(2);
+                    metadata.Delta(1) = tempData(3);
                 end
+                metadata.Units{1} = char(tempData(4:end)');
+            elseif strfind(metadata.Note{noteIndex}.text, 'AXIS_3') == 1
+                % vertical axis
+                tempData = sscanf(metadata.Note{noteIndex}.text(7:end), ' %d %g %g %s');
+                axisType = tempData(1);
+                if axisType == AXT_D
+                    metadata.Origin(2) = tempData(2);
+                    metadata.Delta(2) = tempData(3);
+                end
+                metadata.Units{2} = char(tempData(4:end)');
+            elseif strfind(metadata.Note{noteIndex}.text, 'AXIS_4') == 1
+                % z axis
+                tempData = sscanf(metadata.Note{noteIndex}.text(7:end), ' %d %g %g %s');
+                axisType = tempData(1);
+                if axisType == AXT_D
+                    metadata.Origin(3) = tempData(2);
+                    metadata.Delta(3) = tempData(3);
+                end
+                metadata.Units{3} = char(tempData(4:end)');
+            elseif strfind(metadata.Note{noteIndex}.text, 'AXIS_9') == 1
+                tempData = sscanf(metadata.Note{noteIndex}.text(7:end), ' %d %g %g %s');
+                axisType = tempData(1);
+                if axisType == AXT_RGB
+                    metadata.Origin(4) = tempData(2);
+                    metadata.Delta(4) = tempData(3);
+                    metadata.Units{4} = char(tempData(4:end)');
+                end
+            elseif strfind(metadata.Note{noteIndex}.text, 'INFO_FRAME_RATE') == 1
+                metadata.FramesPerSecond = sscanf(metadata.Note{noteIndex}.text(14:end), ' %d');
+            elseif strfind(metadata.Note{noteIndex}.text, 'INFO_OBJECTIVE_NAME = ') == 1
+                metadata.Objective = metadata.Note{noteIndex}.text(23:end);
+                metadata.Objective = metadata.Objective(metadata.Objective ~= char(0));
+            elseif strfind(metadata.Note{noteIndex}.text, 'PIC_FF_VERSION = ') == 1
+                metadata.FormatVersion = str2double(metadata.Note{noteIndex}.text(18:end));
             else
-                % add info about other note types here
+                % add any note you care about here
 
             end
-            noteIndex = noteIndex + 1;
-        end
-    end
+        else
+            % add info about other note types here
 
-    function note = readNote(index)
-        fseek(fid, notesOffset + (index - 1) * NOTE_LEN, 'bof');
-        note.level = fread(fid, 1, 'int16');
-        note.next = fread(fid, 1, 'int32');
-        note.num = fread(fid, 1, 'int16');
-        note.status = fread(fid, 1, 'int16');
-        note.type = fread(fid, 1, 'int16');
-        note.x = fread(fid, 1, 'int16');
-        note.y = fread(fid, 1, 'int16');
-        note.text = char(fread(fid, 80, 'char')');
+        end
+        noteIndex = noteIndex + 1;
     end
+end
+
+function note = readNote(fid,index,notesOffset)
+	NOTE_LEN    = 96;
+
+    fseek(fid, notesOffset + (index - 1) * NOTE_LEN, 'bof');
+    note.level = fread(fid, 1, 'int16');
+    note.next = fread(fid, 1, 'int32');
+    note.num = fread(fid, 1, 'int16');
+    note.status = fread(fid, 1, 'int16');
+    note.type = fread(fid, 1, 'int16');
+    note.x = fread(fid, 1, 'int16');
+    note.y = fread(fid, 1, 'int16');
+    note.text = char(fread(fid, 80, 'char')');
 end
