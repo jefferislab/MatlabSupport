141a18748a396bc3875ecb6add810a482efbe4ae
Gregory Jefferis <jefferis@gmail.com>
2010-06-11 19:46:10 +0100

    more sensible default include and lib locations

--- teem/compilethis.m
+++ teem/compilethis.m
@@ -1,3 +1,3 @@
-mex nrrdLoad.c -I/projects/lmi/people/gk/teem/include -L/projects/lmi/people/gk/teem/solaris/lib -lteem -lz -lm
-mex nrrdLoadOrientation.c -I/projects/lmi/people/gk/teem/include -L/projects/lmi/people/gk/teem/solaris/lib -lteem -lz -lm
-mex nrrdSave.c -I/projects/lmi/people/gk/teem/include -L/projects/lmi/people/gk/teem/solaris/lib -lteem -lz -lm
+mex nrrdLoad.c -I/usr/local/include -L/usr/local/lib/teem/ -lteem -lz -lm
+mex nrrdLoadOrientation.c -I/usr/local/include -L/usr/local/lib/teem/ -lteem -lz -lm
+mex nrrdSave.c -I/usr/local/include -L/usr/local/lib/teem/ -lteem -lz -lm
