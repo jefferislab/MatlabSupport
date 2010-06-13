d6130dc92fcfc1d554bf5da32ffdcfd93d8fde4e
Gregory Jefferis <jefferis@gmail.com>
2010-06-13 18:12:27 +0100

    Fix teem paths in compilethis for mac 64 bit
    * wrong manual path for 64 bit libs, 
      so 32 bit in /usr/local/lib were being linked instead
    * still haven't figured out how to make fat binaries with both archs
      to get the 64 bit one I had to set TEEM_32BIT=0 manually by 
      find and replace in the build dir.

--- teem/compilethis.m
+++ teem/compilethis.m
@@ -1,7 +1,7 @@
 if	strcmp('MACI64',computer)
-mex nrrdLoad.c -I/usr/local64/include -L/usr/local64/lib/teem/ -lteem -lz -lm
-mex nrrdLoadOrientation.c -I/usr/local64/include -L/usr/local64/lib/teem/ -lteem -lz -lm
-mex nrrdSave.c -I/usr/local64/include -L/usr/local64/lib/teem/ -lteem -lz -lm
+mex nrrdLoad.c -I/usr/local64/include -L/usr/local64/lib/ -lteem -lz -lm
+mex nrrdLoadOrientation.c -I/usr/local64/include -L/usr/local64/lib/ -lteem -lz -lm
+mex nrrdSave.c -I/usr/local64/include -L/usr/local64/lib/ -lteem -lz -lm
 elseif strcmp('x86_64-unknown-linux-gnu',computer)
 	mex nrrdLoad.c -I/public/octave/teem/include -L/public/octave/teem/lib/ -lteem -lz -lm
 	mex nrrdLoadOrientation.c -I/public/octave/teem/include -L/public/octave/teem/lib/ -lteem -lz -lm
